b00836904ffbe8f4031d88dbf6afdc5d5af36a85
Ling Lin <taellinglin@gmail.com>
2023-07-02 18:18:05 -0500

    Ling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 extern crate rtaudio;
+extern crate egui;
+extern crate egui_tiles;
+extern crate eframe;
 mod engine;
-
-
+mod ui;
+use ui::main_panel::*;
 use engine::audio::AudioEngine;
 
-fn main() {
+fn main() -> Result<(), eframe::Error> {
     let sample_rate = 192000;
     let buffer_length = 2048;
 
     let audio_engine = AudioEngine::initialize_audio(sample_rate, buffer_length);
-
-    // ... Rest of your program logic ...
-
+    if let Err(err) = ui::main_panel::main_panel() {
+        eprintln!("Application error: {}", err);
+    }
+    
     // Check for errors
     if let Some(error) = audio_engine.check_for_errors() {
         eprintln!("Audio error: {}", error);
     }
+
+    Ok(())
 }
