0f4ee80cf485626eb3701405642be308157e937b
Ling Lin <taellinglin@gmail.com>
2023-07-03 12:30:31 -0500

    Revert "Ling"
    This reverts commit b00836904ffbe8f4031d88dbf6afdc5d5af36a85.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
 extern crate rtaudio;
-extern crate egui;
-extern crate egui_tiles;
-extern crate eframe;
 mod engine;
-mod ui;
-use ui::main_panel::*;
+
+
 use engine::audio::AudioEngine;
 
-fn main() -> Result<(), eframe::Error> {
+fn main() {
     let sample_rate = 192000;
     let buffer_length = 2048;
 
     let audio_engine = AudioEngine::initialize_audio(sample_rate, buffer_length);
-    if let Err(err) = ui::main_panel::main_panel() {
-        eprintln!("Application error: {}", err);
-    }
-    
+
+    // ... Rest of your program logic ...
+
     // Check for errors
     if let Some(error) = audio_engine.check_for_errors() {
         eprintln!("Audio error: {}", error);
     }
-
-    Ok(())
 }
--- src/ui/main_panel.rs
+++ src/ui/main_panel.rs
@@ -1,159 +1,66 @@
-use eframe::{egui};
-use egui::epaint::{self,Color32};
-use egui::epaint::Hsva;
-#[derive(Debug, Clone)]
-enum TabName {
-    Perform,
-    Arrange,
-    Compose,
-    Instruments,
-    Mixer,
-    Settings,
+use egui::{containers::*, *};
+
+pub struct MainPanel {
+    active_tab: usize,
 }
 
-struct Pane {
-    nr: TabName,
-}
-
-
-
-impl Pane {
-    pub fn new(nr: TabName) -> Self {
-        Self { nr }
+impl MainPanel {
+    pub fn new() -> Self {
+        MainPanel { active_tab: 0 }
     }
 
-    pub fn ui(&mut self, ui: &mut egui::Ui) -> egui_tiles::UiResponse {
-        let hue = match &self.nr {
-            TabName::Perform => 0.0,
-            TabName::Arrange => 60.0,
-            TabName::Compose => 120.0,
-            TabName::Instruments => 180.0,
-            TabName::Mixer => 240.0,
-            TabName::Settings => 300.0,
-        };
+    pub fn ui(&mut self, ui: &mut Ui) {
+        let tabs = vec!["Perform", "Arrange", "Compose", "Instruments", "Mixer", "Settings"];
 
-        let color = egui::epaint::Hsva::new(0.103 * self.nr.clone() as usize as f32, 0.5, 0.5, 1.0);
-        ui.painter().rect_filled(ui.max_rect(), 0.0, color);
-        
-
-        
-
-        let text = match &self.nr {
-            TabName::Perform => "Perform",
-            TabName::Arrange => "Arrange",
-            TabName::Compose => "Compose",
-            TabName::Instruments => "Instruments",
-            TabName::Mixer => "Mixer",
-            TabName::Settings => "Settings",
-        };
-
-        ui.label(format!("The contents of pane {}.", text));
-
-        egui_tiles::UiResponse::None
-    }
-}
-
-
-struct TreeBehavior {}
-
-impl egui_tiles::Behavior<Pane> for TreeBehavior {
-    fn tab_title_for_pane(&mut self, pane: &Pane) -> egui::WidgetText {
-        match &pane.nr {
-            TabName::Perform => egui::WidgetText::from("Perform"),
-            TabName::Arrange => egui::WidgetText::from("Arrange"),
-            TabName::Compose => egui::WidgetText::from("Compose"),
-            TabName::Instruments => egui::WidgetText::from("Instruments"),
-            TabName::Mixer => egui::WidgetText::from("Mixer"),
-            TabName::Settings => egui::WidgetText::from("Settings"),
-        }
-    }
-
-    fn pane_ui(
-        &mut self,
-        ui: &mut egui::Ui,
-        _tile_id: egui_tiles::TileId,
-        pane: &mut Pane,
-    ) -> egui_tiles::UiResponse {
-        pane.ui(ui)
-    }
-}
-
-pub fn main_panel() -> Result<(), eframe::Error> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-
-    let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(320.0, 240.0)),
-        ..Default::default()
-    };
-
-    let mut tree = create_tree();
-
-    eframe::run_simple_native("灵乐", options, move |ctx, _frame| {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            let mut behavior = TreeBehavior {};
-            tree.ui(&mut behavior, ui);
+        // Create the tab bar
+        let response = ui.horizontal(|ui| {
+            for (i, tab) in tabs.iter().enumerate() {
+                let is_active = i == self.active_tab;
+                if ui.tab_button(tab).selected(is_active).clicked() {
+                    self.active_tab = i;
+                }
+            }
         });
-    })
-}
 
-fn create_tree() -> egui_tiles::Tree<Pane> {
-    let mut tiles = egui_tiles::Tiles::default();
-
-    let perform_pane = tiles.insert_pane(Pane::new(TabName::Perform));
-
-    let arrange_timeline_pane = tiles.insert_pane(Pane::new(TabName::Arrange));
-    let arrange_automation_pane = tiles.insert_pane(Pane::new(TabName::Arrange));
-
-    let compose_piano_roll_pane = tiles.insert_pane(Pane::new(TabName::Compose));
-    let compose_channel_editor_pane = tiles.insert_pane(Pane::new(TabName::Compose));
-
-    let instruments_pane = tiles.insert_pane(Pane::new(TabName::Instruments));
-    let mixer_pane = tiles.insert_pane(Pane::new(TabName::Mixer));
-
-    let settings_pane = tiles.insert_pane(Pane::new(TabName::Settings));
-
-    let perform_grid = {
-        let mut children = Vec::new();
-        for _ in 0..144 {
-            children.push(tiles.insert_pane(Pane::new(TabName::Perform)));
+        // Create the tab content
+        match self.active_tab {
+            0 => self.perform_tab(ui),
+            1 => self.arrange_tab(ui),
+            2 => self.compose_tab(ui),
+            3 => self.instruments_tab(ui),
+            4 => self.mixer_tab(ui),
+            5 => self.settings_tab(ui),
+            _ => unreachable!(),
         }
-        tiles.insert_grid_tile(children)
-    };
+    }
 
-    let arrange_container = {
-        let mut children = vec![arrange_timeline_pane, arrange_automation_pane];
-        tiles.insert_vertical_tile(children)
-    };
+    fn perform_tab(&mut self, ui: &mut Ui) {
+        // TODO: Add content for the Perform tab
+        ui.label("Perform Tab");
+    }
 
-    let compose_container = {
-        let mut children = vec![compose_piano_roll_pane, compose_channel_editor_pane];
-        tiles.insert_vertical_tile(children)
-    };
+    fn arrange_tab(&mut self, ui: &mut Ui) {
+        // TODO: Add content for the Arrange tab
+        ui.label("Arrange Tab");
+    }
 
-    let instruments_container = {
-        let mut children = vec![instruments_pane];
-        tiles.insert_vertical_tile(children)
-    };
+    fn compose_tab(&mut self, ui: &mut Ui) {
+        // TODO: Add content for the Compose tab
+        ui.label("Compose Tab");
+    }
 
-    let mixer_container = {
-        let mut children = vec![mixer_pane];
-        tiles.insert_horizontal_tile(children)
-    };
+    fn instruments_tab(&mut self, ui: &mut Ui) {
+        // TODO: Add content for the Instruments tab
+        ui.label("Instruments Tab");
+    }
 
-    let settings_container = {
-        let mut children = vec![settings_pane];
-        tiles.insert_vertical_tile(children)
-    };
+    fn mixer_tab(&mut self, ui: &mut Ui) {
+        // TODO: Add content for the Mixer tab
+        ui.label("Mixer Tab");
+    }
 
-    let mut tabs = Vec::new();
-    tabs.push(perform_grid);
-    tabs.push(arrange_container);
-    tabs.push(compose_container);
-    tabs.push(instruments_container);
-    tabs.push(mixer_container);
-    tabs.push(settings_container);
-
-    let root = tiles.insert_tab_tile(tabs);
-
-    egui_tiles::Tree::new(root, tiles)
+    fn settings_tab(&mut self, ui: &mut Ui) {
+        // TODO: Add content for the Settings tab
+        ui.label("Settings Tab");
+    }
 }
